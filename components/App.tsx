ffbf27d14abb765026e9805ca144fb7a997b9263
Francis <francis.i.authority@gmail.com>
2025-02-14 14:59:28 +0100

    responsiveness added.

--- components/App.tsx
+++ components/App.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { questions } from "@/data/[index"
+import { questions } from "@/data"
 import { useState } from "react"
 
 const App = () => {
@@ -10,21 +10,21 @@ const App = () => {
   }
 
   return (
-    <div className="w-full pt-20 px-4">
+    <div className="w-full pt-20 px-3">
       <div className="w-full grid grid-cols-2 sm:grid-cols-3 justify-items-center gap-2">
         {questions.map(({id, question, answer}) => (
           <div 
             key={id} 
-            className={`h-32 w-full grid col-span-1 items-center rounded-md border-[0.5px] border-gray-600 p-6 
+            className={`h-36 sm:h-40 lg:h-48 w-full grid col-span-1 items-center rounded-md border-[0.5px] border-gray-600 p-6 
               ${selectedCard === id 
-                ? "bg-cyan-500 font-semibold" 
+                ? "bg-green-400 font-semibold" 
                 : "bg-gray-100"}`}
             onClick={() => handleClick(id)}
           >
             {selectedCard !== id ? (
-              <p className="text-center text-xs sm:text-sm md:text-base">{question}</p>
+              <p className="text-center text-sm sm:text-[15px] md:text-base lg:text-lg">{question}</p>
             ) : (
-              <p className="text-center text-xs sm:text-sm md:text-base">{answer}</p>
+              <p className="text-center text-sm sm:text-[15px] md:text-base lg:text-lg">{answer}</p>
             )}
           </div>
         ))}
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,9 @@
 const Header = () => {
   return (
     <div className="absolute top-0 left-0 right-0 w-full bg-black text-white p-5 flex justify-center items-center">
-      <p className="text-base sm:text-lg font-semibold">Flashcards App</p>
+      <p className="text-base sm:text-lg font-semibold">
+        Flashcards App
+      </p>
     </div>
   )
 }
