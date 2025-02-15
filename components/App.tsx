072b5170c519b1c805c00074faffbe9fc19aa39f
Francis <francis.i.authority@gmail.com>
2025-02-15 14:50:59 +0100

    ...

--- components/App.tsx
+++ components/App.tsx
@@ -10,21 +10,21 @@ const App = () => {
   }
 
   return (
-    <div className="w-full pt-20 px-3 pb-3">
+    <div className="w-full pt-20 px-5 pb-3">
       <div className="w-full grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 justify-items-center gap-2">
         {questions.map(({id, question, answer}) => (
           <div 
             key={id} 
             className={`h-36 sm:h-40 lg:h-48 w-full grid col-span-1 items-center rounded-md border-[0.5px] border-gray-600 p-6 
               ${selectedCard === id 
-                ? "bg-cyan-500 font-semibold transition-all duration-500 rotate-clockwise" 
+                ? "bg-yellow-500 font-semibold transition-all duration-500 rotate-clockwise" 
                 : "bg-gray-100 rotate-back duration-300 transition-all"}`}
             onClick={() => handleClick(id)}
           >
             {selectedCard !== id ? (
               <p className="rotate-back text-center text-sm sm:text-[15px] md:text-base lg:text-lg">{question}</p>
             ) : (
-              <p className="rotate-clockwise transition-all duration-100 text-center text-sm sm:text-[15px] md:text-base lg:text-lg">{answer}</p>
+              <p className="rotate-clockwise transition-all duration-100 text-center font-semibold text-sm sm:text-[15px] md:text-base lg:text-lg">{answer}</p>
             )}
           </div>
         ))}
